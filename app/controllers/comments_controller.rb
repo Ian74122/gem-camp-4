6df6cbfeb7c9e06586075ed64dbb487eedc4d160
plok74122 <plok74122@gmail.com>
2023-06-29 11:42:01 +0800

    done edit and update do some refactor

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_post
+  before_action :set_comment, only: [:edit, :update]
 
   def index
     @comments = @post.comments
@@ -10,7 +11,7 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = @post.comments.new(params.require(:comment).permit(:content))
+    @comment = @post.comments.new(comment_params)
     if @comment.save
       flash[:notice] = 'Comment created successfully'
       redirect_to post_comments_path(@post)
@@ -20,9 +21,29 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @comment.update(comment_params)
+      flash[:notice] = 'Comment update successfully'
+      redirect_to post_comments_path(@post)
+    else
+      flash.now[:alert] = 'Comment update failed'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_post
     @post = Post.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end

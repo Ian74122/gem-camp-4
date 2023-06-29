06a99c55fe321b5a21f427b3d8d2d09ddf346f49
plok74122 <plok74122@gmail.com>
2023-06-29 10:11:19 +0800

    refactor post to set post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -18,16 +20,11 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(params.require(:post).permit(:title, :content))
       flash[:notice] = 'Post update successfully'
       redirect_to posts_path
@@ -38,9 +35,14 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     flash[:notice] = 'Post destroyed successfully'
     redirect_to posts_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end

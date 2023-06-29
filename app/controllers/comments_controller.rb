5ed5efe2923e69bfc208bdff406bb0466a072d30
plok74122 <plok74122@gmail.com>
2023-06-29 10:57:48 +0800

    refactor @post in to before action finish new and create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,28 @@
 class CommentsController < ApplicationController
+  before_action :set_post
+
   def index
-    @post = Post.find(params[:post_id])
     @comments = @post.comments
+  end
+
+  def new
+    @comment = @post.comments.new
+  end
+
+  def create
+    @comment = @post.comments.new(params.require(:comment).permit(:content))
+    if @comment.save
+      flash[:notice] = 'Comment created successfully'
+      redirect_to post_comments_path(@post)
+    else
+      flash.now[:alert] = 'Comment create failed'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end

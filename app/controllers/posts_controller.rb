7d8654380f78ba061633c8afd3f58b62e794ff8f
plok74122 <plok74122@gmail.com>
2023-07-04 10:51:20 +0800

    handle post new action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -64,7 +64,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids: [])
+    params.require(:post).permit(:title, :content, :image, category_ids: [])
   end
 
   def set_post

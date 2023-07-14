7afe86373cfd5a6f13da4053f853a1b1ec0c80de
plok74122 <plok74122@gmail.com>
2023-07-14 11:55:35 +0800

    add constraint for admin.com and client.com

--- config/routes.rb
+++ config/routes.rb
@@ -5,27 +5,30 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'welcome#index'
-  resources :posts do
-    resources :comments, except: :show
-  end
-  resources :categories, except: :show
-  resource :user, only: [:show, :edit, :update]
-  namespace :api do
-    namespace :v1 do
-      resources :regions, only: [:index, :show], defaults: { format: :json } do
-        resources :provinces, only: :index
+  constraints(ClientDomainConstraint.new) do
+    resources :posts do
+      resources :comments, except: :show
+    end
+    resources :categories, except: :show
+    resource :user, only: [:show, :edit, :update]
+    namespace :api do
+      namespace :v1 do
+        resources :regions, only: [:index, :show], defaults: { format: :json } do
+          resources :provinces, only: :index
+        end
+        resources :provinces, only: [:index, :show], defaults: { format: :json } do
+          resources :cities, only: :index
+        end
+        resources :cities, only: [:index, :show], defaults: { format: :json } do
+          resources :barangays, only: :index
+        end
+        resources :barangays, only: [:index, :show], defaults: { format: :json }
       end
-      resources :provinces, only: [:index, :show], defaults: { format: :json } do
-        resources :cities, only: :index
-      end
-      resources :cities, only: [:index, :show], defaults: { format: :json } do
-        resources :barangays, only: :index
-      end
-      resources :barangays, only: [:index, :show], defaults: { format: :json }
     end
   end
-
-  namespace :admin do
-    resources :users
+  constraints(AdminDomainConstraint.new) do
+    namespace :admin do
+      resources :users
+    end
   end
 end

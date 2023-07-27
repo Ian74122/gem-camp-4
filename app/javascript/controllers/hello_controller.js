7e2581d3893c82b54dc236c69837f221bac18ce2
plok74122 <plok74122@gmail.com>
2023-07-27 10:58:29 +0800

    add hello controller for stimulus

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,13 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+  static targets = ['name']
+
+  greet() {
+    // this.element.textContent = "Hello World!"
+    const element = this.nameTarget
+    const name = element.value
+    console.log(`Hello, ${name}!`)
+    document.getElementById("welcome_message").innerText = `Hello, ${name}!`
   }
 }
